821664e74ed4de616e66c573e286f49450dc3385
mrmoore6 <mrmoore@dmacc.edu>
2020-10-11 14:21:18 -0500

    Failed

---  test_collections/test_basic_list.py
+++  test_collections/test_basic_list.py
@@ -4,9 +4,9 @@ from fun_with_collections import basic_list
 
 
 class TestList(unittest.TestCase):
-    @patch('fun_with_collections.basic_list.get_input', return_value='15')
+    @patch('fun_with_collections.basic_list.get_input', return_value='5')
     def test_make_list(self, input):
-        self.assertEqual(basic_list.make_list(), [15, 15, 15])
+        self.assertEqual(basic_list.make_list(), [5, 5, 5])
 
 if __name__ == '__main__':
     unittest.main()
--- fun_with_collections/basic_list.py
+++ fun_with_collections/basic_list.py
@@ -1,5 +1,16 @@
+
 def make_list():
-    pass
+    list = []
+    for x in range(0, 3):
+        userInput = int(get_input())
+        list.insert(x, userInput)
+    return list
+
 
 def get_input():
-    pass
+    userInput = input("Enter Value")
+    return userInput
+
+if __name__ == '__main__':
+    print(make_list())
+
