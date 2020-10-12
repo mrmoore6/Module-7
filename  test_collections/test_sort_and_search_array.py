57cb00983118b20d9f43926ba76c07ad9d059cab
mrmoore6 <mrmoore@dmacc.edu>
2020-10-12 16:42:42 -0500

    Array

---  test_collections/test_sort_and_search_array.py
+++  test_collections/test_sort_and_search_array.py
@@ -1,14 +1,14 @@
 import unittest
+from numpy import array
 from fun_with_collections import  sort_and_search_array
 
 class MyTestCase(unittest.TestCase):
     def test_array_search(self):
-        test_Array = [1,7,4,6,5]
+        test_Array = array([1,7,4,6,5])
         self.assertEqual(2, sort_and_search_array.search_array(test_Array)) #inputed 4 to return 2 index
     def test_array_sort(self):
-        test_Array = [6,5,4,3,2,1]
-        expect_Array = [1,2,3,4,5,6]
-        self.assertListEqual(expect_Array, sort_and_search_array.sort_array(test_Array),)
+        test_Array = array([6,5,4,3,2,1])
+        self.assertListEqual([1,2,3,4,5,6], sort_and_search_array.sort_array(test_Array),)
 
 
 
--- fun_with_collections/sort_and_search_array.py
+++ fun_with_collections/sort_and_search_array.py
@@ -5,20 +5,22 @@ Date: 10/11/20
 
 This program sorts an array and search's through it to find inputed value.
 """
-from array import array
+from numpy import array
 def sort_array(arr):
+        arr = arr.tolist()
         arr.sort()
         return arr
 def search_array(num):
     try:
         number = int(input("Enter Value"))
-        num = num.index(number)
+        list = num.tolist()
+        num = list.index(number)
     except ValueError:
         raise ValueError
     return num
 
 if __name__ == '__main__':
-    first_array = [2,4,5,5,6]
+    first_array = array([2,4,5,5,6])
     print(sort_array(first_array))
     print(search_array(first_array))
 
